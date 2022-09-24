21f06dd5dcf617ad218106d2951de6823f3a80b9
jibril-hub <74617210+jibril-hub@users.noreply.github.com>
2022-09-24 12:01:02 +0200

    Ajoutez des fichiers projet.

---
+++ Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace winformTEST
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void TodoListComponent_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var formPopup = new Form();
+            formPopup.Show(this); // if you need non-modal window
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listView1.Items.Add(textBox1.Text);
+        }
+    }
+}
